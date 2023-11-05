652944520ee2b91569081e240a2e4974f3ac274c
suraiyasultana826 <suraiyasultana826@gmail.com>
2023-11-05 16:41:45 +0600

    added button

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import TabComponents from '../../TabComponents/TabComponents';
 import banner from '../../assets/banner.jpg'
 const Home = () => {
@@ -9,8 +10,8 @@ const Home = () => {
           <div>
             <h1 className="text-5xl font-bold">Start your Dream <br /> job from Here</h1>
             <p className="py-6">Total 6,500+ available jobs here, We are damn sure someone <br /> which fits you most. So, let grab the chance to get a dream job.</p>
-            <button className="btn bg-blue-400">Get a job</button>
-            <button className="btn btn-ghost ml-4">Post a job</button>
+            <Link to='/findJob'><button className="btn bg-blue-400">Get a job</button></Link>
+           <Link to='/postJob'> <button className="btn btn-ghost ml-4">Post a job</button></Link>
           </div>
         </div>
       </div>
